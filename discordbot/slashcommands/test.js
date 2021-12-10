ba2fbdddd82e37d02a86e9e3addd282ac88fa316
Baltraz <86964184+Killermaschine88@users.noreply.github.com>
2021-12-10 09:56:06 +0000

    yeet

--- discordbot/slashcommands/test.js
+++ discordbot/slashcommands/test.js
@@ -10,8 +10,10 @@ module.exports = {
   name: "test",
   async execute(interaction) {
 
+    const menu = await interaction.editReply('Starting Minecraft Bot')
+
     bot = mineflayer.createBot({
-  host: 'hub.lemoncloud.net',
+  host: interaction.options.getString('ip') || 'hub.lemoncloud.net',
   username: process.env['minecraft-email'],
   password: process.env['minecraft-password'],
       version: "1.8.9"
@@ -25,37 +27,12 @@ module.exports = {
       mineflayerViewer(bot, { port: 3007, firstPerson: true })
       interaction.editReply({ content: "Ready to be viewed on your [Browser](https://Minecraft-to-Discord.baltrazz.repl.co)"})
       
-      await sleep(3000)
-      await bot.chat('/l')
-      await sleep(3000)
-      await bot.chat('/play sb')
-      
     })
-
-    if(!hasChatted) {
-    bot.on("message", (message) => {
-      hasChatted = true
-      
-      if(message.extra?.length > 0) {
-        console.log(message.extra[0].text)
-      }
-    })
-    }
-
-    if(type === "chat") {
-      if(input) {
-        await bot.chat(`${input}`)
-        interaction.editReply(`said ${input}`)
-      } else {
-        return interaction.editReply("input required")
-      }
-    } else if(type === "move") {
-      if(!movement) return interaction.editreply('movement needed')
-      bot.setControlState(movement, true)
+    
+   // bot.on("message", (message) => {})
+      /*bot.setControlState(movement, true)
       await sleep(1350)
-      bot.clearControlStates()
-      return interaction.editReply(`moved ${movement}`)
-    }
+      bot.clearControlStates()*/
     
   }
 }
