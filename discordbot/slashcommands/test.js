20d9a5d0c4ddcaad1ed83dec69c59fb0712a6a54
Baltraz <86964184+Killermaschine88@users.noreply.github.com>
2021-12-10 10:05:46 +0000

    feat: button rows

--- discordbot/slashcommands/test.js
+++ discordbot/slashcommands/test.js
@@ -2,10 +2,6 @@ const Discord = require('discord.js')
 const mineflayer = require('mineflayer')
 const mineflayerViewer = require('prismarine-viewer').mineflayer
 
-let isRunning = false;
-let bot;
-let hasChatted = false
-
 module.exports = {
   name: "test",
   async execute(interaction) {
@@ -18,6 +14,29 @@ module.exports = {
   password: process.env['minecraft-password'],
       version: "1.8.9"
 })
+
+    const row1 = new Discord.MessageActionRow()
+    .addComponents(
+      new Discord.MessageButton()
+      .setEmojo('a')
+      .setCustomId('leftclick')
+      .setStyle('PRIMARY')
+
+      new Discord.MessageButton()
+      .setEmojo('a')
+      .setCustomId('forward')
+      .setStyle('PRIMARY')
+
+    new Discord.MessageButton()
+      .setEmojo('a')
+      .setCustomId('leftclick')
+      .setStyle('PRIMARY')
+    )
+
+    const row2 = new Discord.MessageActionRow()
+    .addComponents(
+      //
+    )
  
     bot.once("spawn", async () => {
       //Setting bot values
@@ -25,9 +44,13 @@ module.exports = {
       bot.settings.colorsEnabled = false
       
       mineflayerViewer(bot, { port: 3007, firstPerson: true })
-      interaction.editReply({ content: "Ready to be viewed on your [Browser](https://Minecraft-to-Discord.baltrazz.repl.co)"})
-      
+      interaction.editReply({ content: "Ready to be viewed on your [Browser](https://Minecraft-to-Discord.baltrazz.repl.co)", components: [row1, row2] })
     })
+
+    const collector = menu.createMessageComponentCollector({
+			componentType: 'BUTTON',
+			time: 858000,
+		});
     
    // bot.on("message", (message) => {})
       /*bot.setControlState(movement, true)
