b967f95ccab1845f2d3e321f03032a6b479fec89
Baltraz <86964184+Killermaschine88@users.noreply.github.com>
2021-12-29 23:21:06 +0000

    added ign

--- discordbot/slashcommands/fragbot.js
+++ discordbot/slashcommands/fragbot.js
@@ -13,7 +13,7 @@ module.exports = {
       interaction.editReply('starting . . .')
     }
 
-    global.allowed_names = ['baltrazyt', 'itsj4s0n', 'altpapier', 'mattthecuber', 'skyrats']
+    global.allowed_names = ['baltrazyt', 'itsj4s0n', 'altpapier', 'mattthecuber', 'skyrats', 'f0kiwastaken']
     let sent = false
     let in_party = false
     let locationCheck = false
